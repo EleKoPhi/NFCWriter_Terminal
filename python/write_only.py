badae1ced95db44371a0adebe99f409a2d7134f7
Philipp Mochti <philippmochti@Philipps-MacBook-Pro.fritz.box>
2020-12-11 15:17:40 +0100

    Initial commit

---
+++ python/write_only.py
@@ -0,0 +1,200 @@
+import serial.tools.list_ports
+
+
+def IsNewCardPresent():
+    Serial.flushOutput()
+    Serial.write(b'IsNewCardPresent')
+    _ans = Serial.readline().decode().replace("\n", "").replace("\r", "")
+    if 'IsNewCardPresent' in _ans:
+        return Serial.readline().decode().replace("\n", "").replace("\r", "")
+
+    else:
+        return -1
+
+
+def getId():
+    Serial.flushOutput()
+    Serial.flushInput()
+    Serial.write(b'getID')
+    _ans = Serial.readline().decode().replace("\n", "").replace("\r", "")
+    if 'getID' in _ans:
+        ID = Serial.readline().decode().replace("\n", "").replace("\r", "")
+        Serial.flushOutput()
+        Serial.flushInput()
+        return ID
+    else:
+        Serial.flushOutput()
+        Serial.flushInput()
+        return -1
+
+
+def ReadCredit():
+    Serial.flushOutput()
+    Serial.flushInput()
+    Serial.write(b'ReadCredit')
+    _ans = Serial.readline().decode().replace("\n", "").replace("\r", "")
+    if 'ReadCredit' in _ans:
+        credit = Serial.readline().decode().replace("\n", "").replace("\r", "")
+        Serial.flushOutput()
+        Serial.flushInput()
+        return credit
+    else:
+        Serial.flushOutput()
+        Serial.flushInput()
+        return -1
+
+
+def WriteCredit(number):
+    Serial.flushOutput()
+    Serial.flushInput()
+    Serial.write(b'WriteCredit')
+
+    _ans = Serial.readline().decode().replace("\n", "").replace("\r", "")
+    if 'WriteCredit' in _ans:
+        reply = Serial.readline().decode().replace("\n", "").replace("\r", "")
+        if "Input:" in reply:
+            print("Writer is wating for input...")
+            Serial.flushOutput()
+            Serial.flushInput()
+            Serial.write(number.encode())
+            res_write = Serial.readline().decode().replace("\n", "").replace("\r", "")
+            print("Wrote " + str(res_write) + " to card")
+            return res_write
+    else:
+        Serial.flushOutput()
+        Serial.flushInput()
+        return -1
+
+
+def flush_serial():
+    Serial.flushInput()
+    Serial.flushOutput()
+
+
+def reset_card():
+    Serial.flushOutput()
+    Serial.flushInput()
+    Serial.write(b'Reset')
+    _ans = Serial.readline().decode().replace("\n", "").replace("\r", "")
+    if 'Reset' in _ans:
+        Ans = Serial.readline().decode().replace("\n", "").replace("\r", "")
+        if 'Reset' in Ans: print("Trying to reset the chip!")
+        State = Serial.readline().decode().replace("\n", "").replace("\r", "")
+        if 'Err' in State:
+            print("Reset Failed !")
+            return -1
+
+        return 1
+    else:
+        Serial.flushOutput()
+        Serial.flushInput()
+        return -1
+
+
+def read_default():
+    Serial.readline().decode()
+    print("Clear input and output buffer")
+
+
+print("K20 - Terminal\n")
+
+ports = serial.tools.list_ports.comports()
+PortCount = 0
+
+for port in ports:
+    print(str(PortCount) + ": " + port.device)
+    PortCount += 1
+
+Count = int(input("\nChoose: "))
+
+print("connect to: " + ports[Count].device)
+Serial = serial.Serial(ports[Count].device)
+read_default()
+
+print("Karte auflegen")
+
+while True:
+    Serial.flushInput()
+    state = IsNewCardPresent()
+    Serial.flushOutput()
+
+    if state == (" " or "" or not "1"): continue
+
+    if state == 'Unknown cmd':
+        print("Err in serial connection - retry")
+        flush_serial()
+        continue
+
+    if state == "1":
+
+        User_ID = getId()
+        current_credit = ReadCredit()
+
+        print("ID:     " + str(User_ID))
+        print("Credit: " + str(current_credit))
+        print("")
+
+        if str(current_credit) == "-1":
+            print("Card not initialized !")
+            ini = int(input("Initialize(0) // Try again(1): "))
+            if ini is not 1:
+                if reset_card() is not 1:
+                    print("Let's try again...")
+                    continue
+            else:
+                continue
+
+        while True:
+
+            mode = input('1 - 30ct, 2 - 50ct, 3 - Service: ')
+
+            if mode == "1":
+                money = float(input("Money: "))
+                credit = int(money / 0.30 + int(current_credit))
+                break
+
+            elif mode == "2":
+                money = float(input("Money: "))
+                credit = int(money / 0.50 + int(current_credit))
+                break
+
+            elif mode == "3":
+                credit = int(input("Service Credit: "))
+                break
+
+            else:
+                print("Err - Try again")
+                continue
+
+        flush_serial()
+        print("Write " + str(credit) + " to card")
+        write_state = WriteCredit(str(credit))
+
+        flush_serial()
+        i = 0
+        while True:
+
+            if IsNewCardPresent() == "1":
+                break
+
+            i = i + 1
+
+            print("Checking :" + str(i))
+            flush_serial()
+            if (i > 5):
+                break
+
+        if i > 15:
+            print("Something went wrong - Add card again")
+            continue
+
+        flush_serial()
+
+        print("Read new credit: " + ReadCredit())
+
+        if input("Add more? [y][n]") == "y":
+            continue
+        else:
+            break
+
+Serial.close()
---
+++ src/main.cpp
@@ -0,0 +1,125 @@
+#include "Arduino.h"
+#include <MFRC522.h>
+
+#define SS_PIN          D2         
+#define RST_PIN         D1      
+#define YOUR_PAGE       0x4
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
+byte PSWBuff[] = {0xFF, 0xAB, 0xBA, 0xFF};
+byte pACK[] = {0xE, 0x5};
+byte WBuff[] = {0x00, 0x00, 0x00, 0x04};
+byte RBuff[18];
+uint8 Page = YOUR_PAGE;
+
+int ReadCredit()
+{
+  byte buffer[18];
+  byte byteCount;
+  byteCount = sizeof(buffer);
+  int res = mfrc522.MIFARE_Read(Page, buffer, &byteCount);
+  if (res != 0)
+  {
+    return -1;
+  }
+  return (buffer[0]+buffer[1]+buffer[2]+buffer[3])/4; 
+}
+
+String getID()
+{
+  long code = 0;
+
+  if (mfrc522.PICC_ReadCardSerial())
+  {
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+      code = ((code + mfrc522.uid.uidByte[i]) * 10);
+    }
+  }
+  return String(code, DEC);
+}
+
+int WriteCredit(int newCredit_lokal)
+{
+  byte WBuff[] = {newCredit_lokal, newCredit_lokal, newCredit_lokal, newCredit_lokal};
+  mfrc522.PCD_NTAG216_AUTH(&PSWBuff[0], pACK);
+  mfrc522.MIFARE_Ultralight_Write(Page, WBuff, 4); 
+  Serial.println(newCredit_lokal);
+  return 1;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  while(Serial.available()){Serial.read();}
+}
+
+void loop()
+{
+
+  String cmd = Serial.readString();
+  Serial.println("Received: " + cmd);
+
+  if (cmd == "IsNewCardPresent")
+  {
+    Serial.println(mfrc522.PICC_IsNewCardPresent());
+  }
+  else if (cmd == "getID")
+  {
+    Serial.println(getID());
+  }
+  else if (cmd == "ReadCredit")
+  {
+    Serial.println(ReadCredit());
+  }
+  else if (cmd == "WriteCredit")
+  {
+    Serial.println("Input:");
+    String Nr = Serial.readString();
+    Serial.println(WriteCredit(Nr.toInt()));
+  }
+  else if (cmd == "Reset")
+  {
+    while (true)
+    {
+      mfrc522.PICC_ReadCardSerial();
+      ReadCredit();
+      Serial.println("Reset");
+      WriteCredit(0);
+
+      int flag = 0;
+
+      flag = mfrc522.MIFARE_Ultralight_Write(0xE5, PSWBuff, 4);
+      mfrc522.MIFARE_Ultralight_Write(0xE6, pACK, 4);
+      mfrc522.MIFARE_Ultralight_Write(0xE3, WBuff, 4);
+      ReadCredit();
+
+      if (flag == 3)
+      {
+        delay(2000);
+        Serial.println("Err");
+        break;
+      }
+
+      bool flagNow = true;
+      bool flagPast = false;
+
+      while (true)
+      {
+        flagPast = flagNow;
+        flagNow = mfrc522.PICC_IsNewCardPresent();
+        if (flagNow == flagPast)
+          break;
+        Serial.println("Done!");
+      }
+      break;
+    }
+  }
+  else
+  {
+    Serial.println("Unknown cmd");
+  }
+}
